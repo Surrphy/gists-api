e0ccd2aa45d9c006318002cde57ee052e5175774
Surrphy <knrdkine@surrphy.me>
2021-09-16 21:20:39 +0200

    implemented simple gist editing

--- components/GistEdit.jsx
+++ components/GistEdit.jsx
@@ -3,29 +3,50 @@ import { getToken, GistApiWrapper } from "../lib/lib";
 
 export default function GistEdit({gist}) {
     const [token, setToken] = useState('')
-    const [newGist, setNewGist] = useState({filename: '', content: '', descritpion: '', public: false})
-
-    const filenameRef = useRef(null)
+    const [file, setFile] = useState('')
+    const [newGist, setNewGist] = useState({filename: '', content: '', description: ''})
 
     useEffect(() => {
         getToken(setToken)
     }, [])
 
-    const updateGist = () => {
+    const updateGist = (e) => {
+        e?.preventDefault()
+
         let wrapper = new GistApiWrapper(token)
-        setNewGist({ ...newGist, filename: filenameRef})
+
+        let payload = {
+            files: {
+                [file]: {
+                    'content': newGist.content
+                }
+            },
+            'description': newGist.description,
+        }
+
+        wrapper.updateGist(gist.id, payload).catch((e) => console.log(e))
+
+        setNewGist({filename: '', content: '', description: ''})
     }
 
-
+    const deleteFile = () => {
+        setNewGist({...newGist, description: gist.description})
+        updateGist()
+    }
 
     return (
         <div>
-            <form >
+            <form onSubmit={(e) => updateGist(e)}>
                 <label>Filename</label>
-                <select ref={filenameRef}>
+                <select value={file} onChange={(e) => setFile(e.target.value)}>
                     {gist ? Object.keys(gist.files).map((e, i) => (<option value={e} key={i}>{e}</option>)) : ''}
                 </select>
-                
+                <label>Content</label>
+                <input type="text" value={newGist.content} onChange={(e) => setNewGist({...newGist, content: e.target.value})}/>
+                <label>Description</label>
+                <input type="text" value={newGist.description} onChange={(e) => setNewGist({...newGist, description: e.target.value})}/>
+                <input type="submit" />
+                <button onClick={deleteFile}>Delete File</button>
             </form>
         </div>
     )
--- pages/gist/[id].jsx
+++ pages/gist/[id].jsx
@@ -7,17 +7,16 @@ import GistEdit from "../../components/GistEdit";
 export default function Gist() {
     const [token, setToken] = useState('')
     const [gist, setGist] = useState(null)
+    const [file, setFile] = useState('')
     
     const router = useRouter()
-
-    const filenameRef = useRef(null)
 
     const getGist = async () => {
         let wrapper = new GistApiWrapper(token)
 
         let response = await wrapper.getGist(router.query.id).then(res => res.data).catch(e => console.log(e))
         setGist(response)
-        console.log(gist)
+        setFile(Object.keys(response.files)[0])
     }
 
     useEffect(() => {
@@ -27,13 +26,13 @@ export default function Gist() {
     return (
         <div>
             <h4>Filename</h4>
-            <select ref={filenameRef}>
+            <select value={file} onChange={(e) => setFile(e.target.value)}>
                 {gist ? Object.keys(gist.files).map((e, i) => (<option value={e} key={i}>{e}</option>)) : ''}
             </select>
             <h4>Description</h4>
             <p>{gist?.description}</p>
             <h4>Content</h4>
-            <p>{gist?.files[filenameRef]?.content}</p>
+            <p>{gist?.files[file]?.content}</p>
             <h4>Public?</h4>
             <p>{gist?.public ? 'Yes' : 'No'}</p>
             <p><a href={gist?.html_url}>Go to gist</a></p>
