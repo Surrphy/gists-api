e257fa15a8308d8508483e5e177c934eb854b144
Surrphy <knrdkine@surrphy.me>
2021-09-23 18:30:52 +0200

    added simple tests

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,9 +1,17 @@
 import { GistApiWrapper } from "../lib/lib";
-import token from './token.private'
 
-test('GistApiWrapper get request', async () => {
-    let wrapper = new GistApiWrapper(token())
-    let res = await wrapper.getAllGists()
+describe("GistApiWrapper", () => {
+    it("token should be accessible from outside of the class", () => {
+        const token = "dzialajacy_token"
+        const wrapper = new GistApiWrapper(token)
 
-    expect(res.status).toBe(true)
-})
+        return expect(wrapper.token).toEqual(token)
+    })
+
+    it("client should have token in headers", () => {
+        const token = "dzialajacy_token"
+        const wrapper = new GistApiWrapper(token)
+
+        return expect(wrapper.client.defaults.headers.Authorization).toEqual(`token ${token}`)
+    })
+});
